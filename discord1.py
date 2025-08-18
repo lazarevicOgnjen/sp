976e416d0691dcd6403c36643f58cad343f03ec1
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-08-18 23:42:37 +0200

    Update discord1.py

--- discord1.py
+++ discord1.py
@@ -1,17 +1,14 @@
 import os
-from dhooks import Webhook, Embed, File
+from dhooks import Webhook, Embed
 
-image2_path = 'cs elfak.jpg'
 
 WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN1')]
 for url in WEBHOOK_URL:
     hook = Webhook(url)
 
     embed = Embed(
-        description="**[SP main page link - click here -](https://cs.elfak.ni.ac.rs/nastava/course/view.php?id=9)**",
+        description="**[SP glavna stranica je azurirana - click here -](https://cs.elfak.ni.ac.rs/nastava/course/view.php?id=9)**",
         color=0x3498DB
     )
 
-    embed.set_image(url="attachment://cs elfak.jpg")
-    file = File(image2_path, name="cs elfak.jpg")
-    hook.send("@everyone 📢 SP", embed=embed, file=file)
+    hook.send("@everyone 📢 SP", embed=embed)
