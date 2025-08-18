7f6d42760f7a186824cb05219e3f4de00ac011c1
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-08-18 20:20:56 +0200

    Update photo1.py

--- photo1.py
+++ photo1.py
@@ -54,7 +54,7 @@ try:
     
     desired_width = max(width, 1200)  
 
-    desired_height = min(height, 10000)
+    desired_height = min(height, 5000)
 
     page_to_scrape.set_window_size(desired_width, desired_height)  
 
